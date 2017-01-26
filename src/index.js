e8683e55784f0b28fe68aa3836c607a184a36713
Taylor Bell <taylorbell@gmail.com>
2017-01-26 16:16:07 -0700

    fix index

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
-import AnchorImport from './components/Anchor'
-import AvatarImport from './components/Avatar'
-import ButtonImport from './components/Button'
-import CardImport from './components/Card'
-import HeadingImport from './components/Heading'
-import IconImport from './components/Icon'
-import LogoImport from './components/Logo'
-import SplitImport from './components/Split'
-import TabsImport from './components/Tabs'
-import WellImport from './components/Well'
+import AnchorSource from './components/Anchor'
+import AvatarSource from './components/Avatar'
+import ButtonSource from './components/Button'
+import CardSource from './components/Card'
+import CheckboxSource from './components/Checkbox'
+import HeadingSource from './components/Heading'
+import IconSource from './components/Icon'
+import InputSource from './components/Input'
+import LoadingSource from './components/Loading'
+import LogoSource from './components/Logo'
+import RadioGroupSource from './components/RadioGroup'
+import SplitSource from './components/Split'
+import SwitchersSource from './components/Switchers'
+import TabsSource from './components/Tabs'
+import TooltipSource from './components/Tooltip'
+import WellSource from './components/Well'
 
-export const Anchor = AnchorImport
-export const Avatar = AvatarImport
-export const Button = ButtonImport
-export const Card = CardImport
-export const Heading = HeadingImport
-export const Icon = IconImport
-export const Logo = LogoImport
-export const Split = SplitImport
-export const Tabs = TabsImport
-export const Well = WellImport
+export const Anchor = AnchorSource
+export const Avatar = AvatarSource
+export const Button = ButtonSource
+export const Card = CardSource
+export const Checkbox = CheckboxSource
+export const Heading = HeadingSource
+export const Icon = IconSource
+export const Input = InputSource
+export const Loading = LoadingSource
+export const Logo = LogoSource
+export const RadioGroup = RadioGroupSource
+export const Split = SplitSource
+export const Switcher = SwitcherSource
+export const Tabs = TabsSource
+export const Tooltip = TooltipSource
+export const Well = WellSource
