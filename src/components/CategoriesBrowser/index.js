1c5788710a84443b8bb509c593b3b28e913915e0
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2017-02-03 23:40:41 +0200

    Static markup for the Browser component

---
+++ src/components/CategoriesBrowser/index.js
@@ -0,0 +1,15 @@
+import React, {PropTypes} from 'react'
+
+import './index.css'
+
+const CategoriesBrowser = () => {
+  return (
+    <div>CategoriesBrowser component</div>
+  )
+}
+
+Card.propTypes = {
+  className: PropTypes.string,
+}
+
+export default CategoriesBrowser
