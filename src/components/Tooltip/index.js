e370d1e4da803d457ce9156d7a0ff19eb7a6ad0e
Taylor Bell <taylorbell@gmail.com>
2017-02-17 15:17:07 -0700

    tooltip stylying

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -1,9 +1,10 @@
 import React, {PropTypes} from 'react'
+import styled from 'styled-components'
 
-const Tooltip = ({children}) => {
+const Tooltip = ({children, className}) => {
   const ttClasses = 'relative db br2 w-100 mt4 pa3 sans-serif navy bg-white eh-input-tooltip'  
   return (
-    <div className={ttClasses}>
+    <div className={`${ttClasses} ${className}`}>
       {children}
     </div>
   )
@@ -12,4 +13,16 @@ Tooltip.propTypes = {
   children: React.PropTypes.node.isRequired
 }
 
-export default Tooltip
+export default styled(Tooltip)`
+  &:before {
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 0 8px 9px 8px;
+    border-color: transparent transparent #fff transparent;
+    content: '';
+    position: absolute;
+    left: 20px;
+    top: -9px;
+  }
+`
