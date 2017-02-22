db94647ff7517cfdd6ec8552122b93eb3b1c08b9
Trevor D. Miller <trevordmiller@icloud.com>
2017-02-22 15:41:35 -0700

    Remove arguments to enherit from machine npm auth

--- scripts/release.js
+++ scripts/release.js
@@ -1,16 +1,13 @@
 const execSync = require('child_process').execSync
-const writeFileSync = require('fs').writeFileSync
 const semver = require('semver')
-const moduleName = process.argv[2]
-const remoteRepo = process.argv[3]
-const npmToken = process.argv[4]
+const moduleName = 'egghead-ui'
+const remoteRepo = 'git@github.com:eggheadio/egghead-ui.git' 
 const packageVersion = require('../package.json').version
 const npmVersion = `${execSync(`npm show ${moduleName} version`)}`
 console.log('npmVersion', npmVersion)
 
 const publish = () => {
   console.log(`Publishing ${packageVersion} to npm`)
-  writeFileSync('.npmrc', `//registry.npmjs.org/:_authToken=${npmToken}\n`)
   execSync('npm publish')
 
   console.log(`Adding git tag v${packageVersion} to repo`)
