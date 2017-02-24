f0ce69944fa5b2c1b1bd44a8e2f8a613f242c38f
Trevor D. Miller <trevordmiller@icloud.com>
2017-02-24 11:09:04 -0700

    Match API to src/components

--- src/index.js
+++ src/index.js
@@ -1,43 +1,37 @@
-import AnchorSource from './components/Anchor'
-import AvatarSource from './components/Avatar'
 import ButtonSource from './components/Button'
 import CardSource from './components/Card'
-// import CategoriesBrowserImport from './components/CategoriesBrowser'
-// import FooterImport from './components/Footer'
 import CourseCardSource from './components/Card/CourseCard'
-import ErrorSource from './components/Error'
 import LessonCardSource from './components/Card/LessonCard'
 import PlaylistCardSource from './components/Card/PlaylistCard'
+// import CategoriesBrowserImport from './components/CategoriesBrowser'
 import CheckboxSource from './components/Checkbox'
+import ErrorSource from './components/Error'
+// import FooterImport from './components/Footer'
 import HeadingSource from './components/Heading'
 import IconSource from './components/Icon'
 import InputSource from './components/Input'
-import LoadingSource from './components/Loading'
+import LogoSource from './components/Logo'
+import ParagraphSource from './components/Paragraph'
+// import PlaylistSource from './components/Playlist'
 import RadioGroupSource from './components/RadioGroup'
 import RequestSource from './components/Request'
-import SplitSource from './components/Split'
-import TabsSource from './components/Tabs'
 import TooltipSource from './components/Tooltip'
-import WellSource from './components/Well'
 
-export const Anchor = AnchorSource
-export const Avatar = AvatarSource
 export const Button = ButtonSource
 export const Card = CardSource
-// export const CategoriesBrowser = CategoriesBrowserImport
-// export const Footer = FooterImport
 export const CourseCard = CourseCardSource
-export const Error = ErrorSource
 export const LessonCard = LessonCardSource
 export const PlaylistCard = PlaylistCardSource
+// export const CategoriesBrowser = CategoriesBrowserImport
 export const Checkbox = CheckboxSource
+export const Error = ErrorSource
+// export const Footer = FooterImport
 export const Heading = HeadingSource
 export const Icon = IconSource
 export const Input = InputSource
-export const Loading = LoadingSource
+export const Logo = LogoSource
+export const Paragraph = ParagraphSource
+// export const Playlist = PlaylistSource
 export const RadioGroup = RadioGroupSource
 export const Request = RequestSource
-export const Split = SplitSource
-export const Tabs = TabsSource
 export const Tooltip = TooltipSource
-export const Well = WellSource
