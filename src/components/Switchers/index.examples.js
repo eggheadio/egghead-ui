c529d9bf3efbe2435bb2b08dd909ad490e16ea91
Taylor Bell <taylorbell@gmail.com>
2017-02-28 10:13:40 -0700

    progress

--- src/components/Switchers/index.examples.js
+++ src/components/Switchers/index.examples.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import {storiesOf} from '@kadira/storybook'
-import StaticSwitcher from '.'
+import StaticSwitcher, { Switcher } from '.'
 
 const decoratorClasses = 'flex flex-column content-center justify-around flex-wrap items-center vh-100 bg-navy'
+
+storiesOf('Switchers')
+  .addDecorator((story) => (<div className={decoratorClasses}>{story()}</div>))
+  .addWithInfo('Switcher', () => (
+    <Switcher leftOption={'This'} rightOption={'That'} leftChecked />
+  ))
 
 storiesOf('Static: Switchers')
   .addDecorator((story) => (<div className={decoratorClasses}>{story()}</div>))
--- src/components/Switchers/index.js
+++ src/components/Switchers/index.js
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
 import styled from 'styled-components'
+
+const labelClasses = 'w-50 normal lh-copy tc light-gray relative z-1 pv2 pointer'
 
 const StyledInput = styled.input`
   &:checked + span {
     transform: translateX(100%);
   }
 `
+
+export class Switcher extends Component {
+  static propTypes = {
+    leftOption: PropTypes.string,
+    rightOption: PropTypes.string,
+    leftChecked: PropTypes.bool,
+    onClick: PropTypes.func
+  }
+
+  constructor(props) {
+    super(props)
+
+    const leftChecked = 'leftChecked' in props ? props.leftChecked : false
+
+    this.state = { leftChecked }
+  }
+
+  handleChange = (e) => {
+    const { leftChecked } = this.state
+    console.log('leftChecked:', leftChecked)
+    this.setState({ leftChecked: !leftChecked })
+  }
+
+  render() {
+    const { rightOption, leftOption, onClick } = this.props
+    const { leftChecked } = this.state
+    
+    return (
+      <div className='w5 relative flex items-center br-pill ba b--light-navy bg-dark-navy'>
+        <label className={`${labelClasses} border-box`} htmlFor='left'>
+          {leftOption}
+        </label>
+        <input type='radio' name={leftOption} id='left' className='absolute o-0' onChange={this.handleChange} />
+        
+        <label className={`${labelClasses} border-box`}>
+          {rightOption}
+        </label>
+        <input type='radio' name={rightOption} id='right' className='absolute o-0' onChange={this.handleChange} />
+
+        <span className='w-50 db br-pill pa2 bg-blue absolute left-0 border-box'
+              style={{
+                transition: '150ms',
+                transform: 'translateX(100%)'
+              }}>
+          <span className='db h2'></span>
+        </span>
+      </div>
+    )
+  }
+}
 
 const StaticSwitcher = () => (
   <div className='w5 relative flex items-center br-pill ba b--light-navy bg-dark-navy'>
