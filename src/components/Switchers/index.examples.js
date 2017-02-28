d6eaf171d55e1253eeab548fdc7050526bcffe03
Taylor Bell <taylorbell@gmail.com>
2017-02-28 11:09:36 -0700

    slider transition

--- src/components/Switchers/index.examples.js
+++ src/components/Switchers/index.examples.js
@@ -1,20 +1,12 @@
 import React from 'react'
 import {storiesOf} from '@kadira/storybook'
-import StaticSwitcher, { Switcher } from '.'
+import Switcher from '.'
 
 const decoratorClasses = 'flex flex-column content-center justify-around flex-wrap items-center vh-100 bg-navy'
 
 storiesOf('Switchers')
   .addDecorator((story) => (<div className={decoratorClasses}>{story()}</div>))
   .addWithInfo('Switcher', () => (
-    <Switcher leftOption={'This'} rightOption={'That'} leftChecked />
+    <Switcher leftOption={'This'} rightOption={'That'}  selectedItem='right' />
   ))
 
-storiesOf('Static: Switchers')
-  .addDecorator((story) => (<div className={decoratorClasses}>{story()}</div>))
-  .addWithInfo('Big', () => (
-    <StaticSwitcher />
-  ))
-  .addWithInfo('Small', () => (
-    <StaticSwitcher />
-  ))
--- src/components/Switchers/index.js
+++ src/components/Switchers/index.js
@@ -1,82 +1,62 @@
 import React, { Component, PropTypes } from 'react'
-import styled from 'styled-components'
 
 const labelClasses = 'w-50 normal lh-copy tc light-gray relative z-1 pv2 pointer'
 
-const StyledInput = styled.input`
-  &:checked + span {
-    transform: translateX(100%);
-  }
-`
+const Highlight = ({selectedItem}) => {
+  return (
+    <span className='w-50 db br-pill pa2 bg-blue absolute left-0 border-box'
+          style={{
+            transition: '150ms',
+            transform: selectedItem === 'right' ? 'translateX(100%)' : ''
+          }}>
+      <span className='db h2' />
+    </span>
+  )
+}
+Highlight.propTypes = {
+  selectedItem: PropTypes.oneOf(['left', 'right']),
+}
 
-export class Switcher extends Component {
+class Switcher extends Component {
   static propTypes = {
     leftOption: PropTypes.string,
     rightOption: PropTypes.string,
-    leftChecked: PropTypes.bool,
+    selectedItem: PropTypes.oneOf(['left', 'right']),
     onClick: PropTypes.func
   }
 
   constructor(props) {
     super(props)
 
-    const leftChecked = 'leftChecked' in props ? props.leftChecked : false
-
-    this.state = { leftChecked }
+    this.state = { selectedItem: this.props.selectedItem === 'left' ? 'left' : 'right' }
   }
 
   handleChange = (e) => {
-    const { leftChecked } = this.state
-    console.log('leftChecked:', leftChecked)
-    this.setState({ leftChecked: !leftChecked })
+    const { selectedItem } = this.state
+    this.setState({ selectedItem: selectedItem === 'left' ? 'right' : 'left' })
   }
 
   render() {
     const { rightOption, leftOption, onClick } = this.props
-    const { leftChecked } = this.state
-    
+    const { selectedItem } = this.state
+
     return (
       <div className='w5 relative flex items-center br-pill ba b--light-navy bg-dark-navy'>
         <label className={`${labelClasses} border-box`} htmlFor='left'>
           {leftOption}
-         <input type='radio' name={leftOption} id='left' className='absolute o-0' checked={leftChecked} onChange={this.handleChange} />
+         <input type='radio' name={leftOption} id='left' className='absolute o-0' checked={selectedItem === 'left'} onChange={this.handleChange} />
         </label>
         
         <label className={`${labelClasses} border-box`}>
           {rightOption}
-          <input type='radio' name={rightOption} id='right' className='absolute o-0' checked={!leftChecked} onChange={this.handleChange} />
+          <input type='radio' name={rightOption} id='right' className='absolute o-0' checked={selectedItem === 'right'} onChange={this.handleChange} />
         </label>
+        
+        <Highlight selectedItem={selectedItem} />
 
-        <span className='w-50 db br-pill pa2 bg-blue absolute left-0 border-box'
-              style={{
-                transition: '150ms',
-                transform: 'translateX(100%)'
-              }}>
-          <span className='db h2'></span>
-        </span>
       </div>
     )
   }
 }
 
-const StaticSwitcher = () => (
-  <div className='w5 relative flex items-center br-pill ba b--light-navy bg-dark-navy'>
-    <label className='w-50 normal lh-copy tc light-gray relative z-1 pv2 pointer'
-           htmlFor='some-id-left'
-           style={{boxSizing: 'border-box'}}>Monthly</label>
-    <input type='radio' name='some-name' id='some-id-left' className='absolute o-0' />
-    <label className='w-50 normal lh-copy tc light-gray relative z-1 pv2 pointer'
-           htmlFor='some-id-right'
-           style={{boxSizing: 'border-box'}}>Weekly</label>
-    <StyledInput type='radio' name='some-name' id='some-id-right' className='absolute o-0' />
-    <span className='w-50 db br-pill pa2 bg-blue absolute left-0'
-          style={{
-            boxSizing: 'border-box',
-            transition: '150ms'
-          }}>
-      <span className='db h2'></span>
-    </span>
-  </div>
-)
-
-export default StaticSwitcher
+export default Switcher
