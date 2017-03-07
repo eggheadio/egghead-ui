f3f0377ec98ca7d426a27ad5214b7a349709979a
Trevor D. Miller <trevordmiller@icloud.com>
2017-03-06 22:04:45 -0700

    Fix loading spinner

--- src/components/Request/components/Loading/index.js
+++ src/components/Request/components/Loading/index.js
@@ -1,14 +1,30 @@
 import React from 'react'
+import styled, {keyframes} from 'styled-components'
 import Icon from '../../../Icon'
 
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Rotate = styled.div`
+  animation: ${rotate360} 1.5s linear infinite;
+`;
+
 const Loading = () => (
-  <div className='mv3 white'>
-    <Icon
-      type='refresh'
-      size='2'
-      spin
-      className='mr2 pa3'
-    />
+  <div className='mv3 white flex items-center'>
+    <div className='mr2'>
+      <Rotate>
+        <Icon 
+          type='refresh'
+          color='white'
+        />
+      </Rotate>
+    </div>
     Loading...
   </div>
 )
