f62e7188279839597baece7c4cd57b610cd58f48
Trevor D. Miller <trevordmiller@icloud.com>
2017-03-22 17:54:26 -0400

    Promote List component from egghead-instructor-center (resolve #73) (#80)
    * Promote List component from egghead-instructor-center (resolve #73)
    
    * v3.4.0

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ErrorSource from './components/Error'
 import HeadingSource from './components/Heading'
 import IconSource from './components/Icon'
 import InputSource from './components/Input'
+import ListSource from './components/List'
 import MarkdownSource from './components/Markdown'
 import ParagraphSource from './components/Paragraph'
 import RadioGroupSource from './components/RadioGroup'
@@ -19,6 +20,7 @@ export const Error = ErrorSource
 export const Heading = HeadingSource
 export const Icon = IconSource
 export const Input = InputSource
+export const List = ListSource
 export const Markdown = MarkdownSource
 export const Paragraph = ParagraphSource
 export const RadioGroup = RadioGroupSource
