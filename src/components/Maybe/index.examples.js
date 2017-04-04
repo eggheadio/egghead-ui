b39d8359a1f9c8f464d66fd01f29d5a75868e097
Trevor D. Miller <trevordmiller@icloud.com>
2017-04-04 12:04:21 -0600

    Add Maybe component (#103)
    * Add Maybe component (resolve #101)
    
    * v4.6.0
    
    * Export Maybe
    
    * v4.6.1

---
+++ src/components/Maybe/index.examples.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {storiesOf} from '@kadira/storybook'
+import {NodeFixture} from '../../utils/fixtures'
+import Maybe from '.'
+
+storiesOf('Maybe')
+  .addWithInfo(
+    'Info',
+    'Used to conditionally render something',
+    () => (
+      <Maybe condition={true}>
+        {NodeFixture}
+      </Maybe>
+    ),
+  )
+  .addWithPropsCombinations(
+    'Combinations',
+    Maybe, 
+    {
+      children: [NodeFixture],
+      condition: [true, false],
+    },
+  )
