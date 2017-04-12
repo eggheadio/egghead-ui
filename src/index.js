c533ce2e43c691c6702a6307db2d488defd18043
Trevor D. Miller <trevordmiller@icloud.com>
2017-04-12 14:19:17 -0600

    Simplify API export logic

--- src/index.js
+++ src/index.js
@@ -1,53 +1,26 @@
-import AnchorSource from './components/Anchor'
-import AvatarSource from './components/Avatar'
-import ButtonSource from './components/Button'
-import CardSource from './components/Card'
-import DeviceWidthSource from './components/DeviceWidth'
-import ErrorSource from './components/Error'
-import HeadingSource from './components/Heading'
-import IconSource from './components/Icon'
-import IconLabelSource from './components/IconLabel'
-import ImageSource from './components/Image'
-import ListSource from './components/List'
-import LoadingSource from './components/Loading'
-import MarkdownSource from './components/Markdown'
-import MaybeSource from './components/Maybe'
-import ParagraphSource from './components/Paragraph'
-import RequestSource from './components/Request'
-import TabsSource from './components/Tabs'
-import ToggleSource from './components/Toggle'
+export Anchor from './components/Anchor'
+export Avatar from './components/Avatar'
+export Button from './components/Button'
+export Card from './components/Card'
+export DeviceWidth from './components/DeviceWidth'
+export Error from './components/Error'
+export Heading from './components/Heading'
+export Icon from './components/Icon'
+export IconLabel from './components/IconLabel'
+export Image from './components/Image'
+export List from './components/List'
+export Loading from './components/Loading'
+export Markdown from './components/Markdown'
+export Maybe from './components/Maybe'
+export Paragraph from './components/Paragraph'
+export Request from './components/Request'
+export Tabs from './components/Tabs'
+export Toggle from './components/Toggle'
 
-import colorsUrlSource from './utils/colorsUrl'
-import createLessonsUrlSource from './utils/createLessonsUrl'
-import hardCodedSizesUrlSource from './utils/hardCodedSizesUrl'
-import instructorMilestonesUrlSource from './utils/instructorMilestonesUrl'
-import lessonStatesSource from './utils/createLessonsUrl'
-import numberFormattingByTypeSource from './utils/createLessonsUrl'
-import urlsSource from './utils/urls'
-
-export const Anchor = AnchorSource
-export const Avatar = AvatarSource
-export const Button = ButtonSource
-export const Card = CardSource
-export const DeviceWidth = DeviceWidthSource
-export const Error = ErrorSource
-export const Heading = HeadingSource
-export const Icon = IconSource
-export const IconLabel = IconLabelSource
-export const Image = ImageSource
-export const List = ListSource
-export const Loading = LoadingSource
-export const Markdown = MarkdownSource
-export const Maybe = MaybeSource
-export const Paragraph = ParagraphSource
-export const Request = RequestSource
-export const Tabs = TabsSource
-export const Toggle = ToggleSource
-
-export const colorsUrl = colorsUrlSource
-export const createLessonsUrl = createLessonsUrlSource
-export const hardCodedSizesUrl = hardCodedSizesUrlSource
-export const instructorMilestonesUrl = instructorMilestonesUrlSource
-export const lessonStates = lessonStatesSource
-export const numberFormattingByType = numberFormattingByTypeSource
-export const urls = urlsSource
+export colorsUrl from './utils/colorsUrl'
+export createLessonsUrl from './utils/createLessonsUrl'
+export hardCodedSizesUrl from './utils/hardCodedSizesUrl'
+export instructorMilestonesUrl from './utils/instructorMilestonesUrl'
+export lessonStates from './utils/createLessonsUrl'
+export numberFormattingByType from './utils/createLessonsUrl'
+export urls from './utils/urls'
