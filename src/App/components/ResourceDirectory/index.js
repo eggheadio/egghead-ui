91fbb872c40ae4be926e9cb7ebda8aa55f38ba56
Trevor D. Miller <trevordmiller@icloud.com>
2017-04-18 15:23:54 -0600

    Polish directory styles

--- src/App/components/ResourceDirectory/index.js
+++ src/App/components/ResourceDirectory/index.js
@@ -3,12 +3,12 @@ import {map} from 'lodash'
 import {Link} from 'react-router-dom'
 
 const ResourceDirectory = ({resources}) => (
-  <nav>
+  <nav className='flex flex-wrap'>
     {map(resources.items, (value, key) => (
       <Link 
         key={key}
         to={`${resources.urlBase}/${key}`}
-        className='db mb2'
+        className='db pa3 mb2 mr2 bg-base-secondary dark-gray no-underline'
       >
         {key}
       </Link>
