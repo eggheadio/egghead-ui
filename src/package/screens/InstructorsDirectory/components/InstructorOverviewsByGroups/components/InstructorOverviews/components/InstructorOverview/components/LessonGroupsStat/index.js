53086b2d7aa988f7c3c431f2f18f0be84b5f903b
Trevor D. Miller <trevordmiller@icloud.com>
2017-04-26 15:31:50 -0600

    Polish instructor overviews

--- src/package/screens/InstructorsDirectory/components/InstructorOverviewsByGroups/components/InstructorOverviews/components/InstructorOverview/components/LessonGroupsStat/index.js
+++ src/package/screens/InstructorsDirectory/components/InstructorOverviewsByGroups/components/InstructorOverviews/components/InstructorOverview/components/LessonGroupsStat/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Heading from 'package/components/Heading'
 
 export default ({label, count}) => (
-  <div className='pv2 ph3 bg-gray-secondary br2 tc'>
+  <div className={`pv2 ph3 bg-gray-secondary br2 tc ${count > 0 ? '' : 'o-30'}`}>
     <Heading level='5'>
       {label}
     </Heading>
