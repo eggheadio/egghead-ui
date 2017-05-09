c5e96e05e461423866de2eb2fcaa9e9a3a2dfa7f
Trevor D. Miller <trevordmiller@icloud.com>
2017-05-09 13:50:25 -0600

    Add log out button when logged in

--- src/App/components/Navigation/index.js
+++ src/App/components/Navigation/index.js
@@ -1,19 +1,39 @@
 import React, {PropTypes} from 'react'
 import {NavLink} from 'react-router-dom'
+import windowMock from 'package/utils/windowMock'
+import Button from 'package/components/Button'
+import logout from 'package/utils/logout'
+
+const universalWindow = typeof(window) === 'undefined' 
+  ? windowMock 
+  : window
 
 const Navigation = ({items}) => (
   <nav className='bg-base-secondary'>
-    {items.map((item) => (
-      <NavLink
-        key={item.label}
-        exact
-        className='flex pa3 white no-underline'
-        activeClassName='orange'
-        to={item.path}
-      >
-        {item.label}
-      </NavLink>
-    ))}
+    <div>
+      {items.map((item) => (
+        <NavLink
+          key={item.label}
+          exact
+          className='flex pa3 white no-underline'
+          activeClassName='orange'
+          to={item.path}
+        >
+          {item.label}
+        </NavLink>
+      ))}
+    </div>
+    <div className='pv4 ph3'>
+      {universalWindow.localStorage.getItem('token')
+        ? <Button 
+            onClick={logout}
+            size='small'
+          >
+            Logout
+          </Button>
+        : null
+      }
+    </div>
   </nav>
 )
 
