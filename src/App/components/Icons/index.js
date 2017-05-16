6d78cb50fd779cede0d653e2313144e010ac6c98
Taylor Bell <taylorbell@gmail.com>
2017-05-16 11:23:17 -0600

    icon grid

---
+++ src/App/components/Icons/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {map} from 'lodash'
+import Icon, { types } from 'package/components/Icon'
+
+const Icons = () => (
+  <div className='flex flex-wrap'>
+    {map(types, (type) => (
+      <div 
+        key={type}
+        className='mr3 mb3 flex items-center'
+      >
+          {
+            <div className='flex flex-column justify-center w5 h3 items-center'>
+              <Icon type={type} />
+              <span className='mt2 f6 code'>{type}</span>
+            </div>
+          }
+      </div>
+    ))}
+  </div>
+)
+
+export default Icons
+
+
